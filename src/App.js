31e0ba2185b2b1d92509166511abe3e35496e0b2
Miran saman <miran.saman.04@gmail.com>
2021-07-23 23:50:05 +0300

    create left bar with rooms

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import Home from "./components/home"
+import React, { useState, useEffect } from 'react';
+import Portfolio from './components/portfolio';
+import About from './components/about';
+import News from './components/news';
+import Contacts from './components/contacts';
 
 function App() {
+  
+  const [room, setRoom] = useState(null)
+  
   return (
     <div className="App">
-      
+      <div className="LeftBar">
+        <div className="manu-bar">
+          <h2>Kurdistan</h2>
+          <div className="manu">
+            <div className="manu-text" onClick={()=> setRoom(<Home/>)}>Home</div>
+            <div className="manu-text" onClick={()=> setRoom(<About/>)}>About</div>
+            <div className="manu-text" onClick={()=> setRoom(<Portfolio/>)}>Portfolio</div>
+            <div className="manu-text" onClick={()=> setRoom(<News/>)}>News</div>
+            <div className="manu-text" onClick={()=> setRoom(<Contacts/>)}>Contacts</div>
+          </div>
+        </div>
+      </div>
+      <div className="push"></div>
+      <div>{room}</div>
     </div>
   );
 }
---
+++ src/components/news.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+
+function News(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default News
