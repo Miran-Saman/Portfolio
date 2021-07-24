8e76d2e62923e4cea0ea3d78abed9fdcaed62dd6
Miran saman <miran.saman.04@gmail.com>
2021-07-24 14:33:14 +0300

    create storage

---
+++ src/components/storage.jsx
@@ -0,0 +1,10 @@
+const HomeStrage = {
+    name : "MIRAN SAMAN",
+    job : "sdmfkasdmkadfasdfkskf",
+    FaceBookLink : "https://www.facebook.com/miran.saman.1612/",
+    TwitterLink : "",
+    InstagramLink : "",
+    DirbbbleLink:""
+}
+
+export default HomeStrage
