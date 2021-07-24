854b2b894877abd018c451267087973d57539232
Miran saman <miran.saman.04@gmail.com>
2021-07-24 22:04:33 +0300

    create about room

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,10 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import "./about.css"
+import Storage from "../storage"
 
 function About(props) {
-    return (
+    const AboutStorage = Storage.AboutStorage
+    const lists = AboutStorage.address.map((number)=> 
         <div>
-            
+            <span className={number.name}>
+                {number.p1}
+            </span>
+            <a style={{textDecoration:"none"}} href={number.EmailLink }>
+                <span className={number.name + "2"}>
+                    {number.p2}
+                </span>
+            </a>
+        </div>
+    )
+    return (
+        <div className="about">
+            <img src="" className="image-about"/>
+            <h1 className="director">{AboutStorage.director}</h1>
+            <div className="description">
+                <div className="short-description">{AboutStorage.shortDescription}</div>
+                <div>
+                    {lists}
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/storage.jsx
+++ src/components/storage.jsx
@@ -6,6 +6,19 @@ const Storage = {
         TwitterLink : "",
         InstagramLink : "",
         DirbbbleLink:""
+    },
+    AboutStorage: {
+        director: "Miran saman",
+        shortDescription: "Hello, I am a programmer in slemany and happy to create a big program",
+        address : [
+            {name : "address",p1 : "Birthday:" , p2 : "1/4/2004"},
+            {name : "address",p1 : "age: " , p2 : "17"},
+            {name : "address",p1 : "address: " , p2 : "slemany, chia, 43, 17"},
+            {name : "addressLink", p1 : "Email:", p2 : "miran@gmail.com", EmailLink: "mailto:miran00saman@gmail.com"},
+            {name : "addressLink", p1 : "Phone:", p2 : "0770 946 6269" , EmailLink: "mailto:miran00saman@gmail.com"},
+            {name : "address", p1 : "study:", p2 : "Slemany high school"},
+            {name : "address", p1 : "Freelance:", p2 : "available"},
+        ]
     }
 }
 
