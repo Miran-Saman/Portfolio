7ed1c7786bf9e40d417d6052766f124f7bc2735b
Miran saman <miran.saman.04@gmail.com>
2021-07-25 12:38:53 +0300

    create two element named title strong

---
+++ src/components/strong.jsx
@@ -0,0 +1,6 @@
+function Strong(props) {
+    return (
+        <strong className="strong">{props.value}</strong>
+    );
+}
+export default Strong
---
+++ src/components/title.jsx
@@ -0,0 +1,9 @@
+function Title(props) {
+    return (
+        <div className="title">
+               <p className="Pportfolio">{props.value}</p>
+        </div>
+    );
+}
+
+export default Title;
