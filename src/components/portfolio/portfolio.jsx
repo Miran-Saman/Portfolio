80ff6ae2a41650193ef7d6dd514bcbc43cc2a33a
Miran saman <miran.saman.04@gmail.com>
2021-07-27 12:18:16 +0300

    lessen code

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -9,7 +9,9 @@ function Portfolio(props) {
 
     const ProtfolioStorage = Storage.ProtfolioStorage;
     const [box, setBox] = useState(0)
-    const BoxList = ProtfolioStorage.map((number)=>  {return})
+    const BoxLists = ProtfolioStorage.map((item,i) => 
+    <span onClick={()=> setBox(i)} className="Box">{item.manu}</span>
+    )
 
     return (
         <div className="portfolio">
@@ -17,11 +19,7 @@ function Portfolio(props) {
           <div className="title-flex">
               <Strong value={"Creative Portfolio"}/>
               <div className="Box-flex">
-                <span onClick={()=> setBox(0)} className="Box">{ProtfolioStorage[0].manu}</span>
-                <span onClick={()=> setBox(1)} className="Box">{ProtfolioStorage[1].manu}</span>
-                <span onClick={()=> setBox(2)} className="Box">{ProtfolioStorage[2].manu}</span>
-                <span onClick={()=> setBox(3)} className="Box">{ProtfolioStorage[3].manu}</span>
-                <span onClick={()=> setBox(4)} className="Box">{ProtfolioStorage[4].manu}</span>
+                {BoxLists}
               </div>
           </div>
           <div className="portfolio-images">
