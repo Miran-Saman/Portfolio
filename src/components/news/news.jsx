7d5fd72115a4246d9cd84e5d51c9d003c68e8ee1
Miran saman <miran.saman.04@gmail.com>
2021-07-28 10:22:23 +0300

    created news

--- src/components/news/news.jsx
+++ src/components/news/news.jsx
@@ -4,10 +4,23 @@ import Title from '../title';
 import Modal from './modal';
 import Storage from "../storage"
 import "./news.css"
+import reactDom from 'react-dom';
+
+function Open(props) {
+    return (
+        <div className="display">
+            <div className="display-border">
+                <img src={props.index} alt="" />
+            </div>
+        </div>
+    );
+}
 
 function News(props) {
-    const newRender = Storage.NewsStorage.map((item , i) => 
-        <div className="bord">
+    const newsRender = Storage.NewsStorage.map((item) => 
+        <div className="bord" onClick={()=> 
+            reactDom.render(<Open index={item.image}/>, document.getElementById("rendering"))
+        }>
             <div className="set-image">
                 <div className="fix-image">
                     <img src={item.image} alt="" className="setting-image" />
@@ -26,7 +39,7 @@ function News(props) {
             <Title value="News"/>
             <Strong value="Get in Touch" />
             <div className="flexer">
-                {newRender}
+                {newsRender}
             </div>
         </div>
     )
