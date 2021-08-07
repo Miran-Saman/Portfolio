83dbe7f5294ee91615f866bb4537c8c634457a3e
Miran saman <miran.saman.04@gmail.com>
2021-08-07 12:38:44 +0300

    update portfolio

--- src/components/portfolio/boxs.jsx
+++ src/components/portfolio/boxs.jsx
@@ -4,32 +4,36 @@ import Storage from "../storage"
 import Display from "./Display";
 import Modal from "react-modal"
 
-const styleModal = {
-    overlay : {
-        position: "fixed",
-        inset: "0px",
-        backgroundColor: "rgb(0 0 0 / 75%)",
-    },
-    content : {
-        position: "unset",
-        inset : "unset",
-        border: "unset",
-        background: "unset",
-        overflow: "unset",
-        borderRadius: "unset",
-        outline: "unset",
-        padding: "unset",
-        display : "flex",
-        justifyContent : "center"
-    }
-} 
 
 function Boxs(props) {
     const [open , setOPen ] = useState(false)
+    const [IO , setIO ] = useState(true)
+    
+    const styleModal = {
+        overlay : {
+            position: "fixed",
+            inset: "0px",
+            backgroundColor: "rgb(0 0 0 / 75%)",
+            animationDuration : ".4s",
+            animationName : IO ? "open" : "close"
+        },
+        content : {
+            position: "unset",
+            inset : "unset",
+            border: "unset",
+            background: "unset",
+            overflow: "unset",
+            borderRadius: "unset",
+            outline: "unset",
+            padding: "unset",
+            display : "flex",
+            justifyContent : "center"
+        }
+    } 
 
     const ProtfolioStorage = Storage.ProtfolioStorage[props.number].index;
     const lists = ProtfolioStorage.map((image)=> 
-    <div className="bord" onClick={() => setOPen([image,ProtfolioStorage])}>
+    <div className="bord" onClick={() => {setIO(true);setOPen([image,ProtfolioStorage])}}>
         <div className="border-of-picture">
             <img src={image} alt="" className="setting-picture"/>
         </div>
@@ -41,7 +45,7 @@ function Boxs(props) {
         <Modal 
             style={styleModal}
             isOpen={open}
-            onRequestClose={()=> setOPen(false)}
+            onRequestClose={()=> {setIO(false);setTimeout(()=> setOPen(false),390)}}
             >
             {open && <Display image={open[0]} ProtfolioStorage={open[1]}/>}
         </Modal>
